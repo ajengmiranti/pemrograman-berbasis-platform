77bc8201c2a2324605d322c3c1d97832eb8ca0ff
Ajeng Miranti <114462198+ajengmiranti@users.noreply.github.com>
2023-10-05 20:48:12 +0700

    tugas-sesi-2

---
+++ profiletugassesi2.js
@@ -0,0 +1,10 @@
+const profile = {
+  firstNama: "Ajeng",
+  lastName: "Miranti",
+  age: 19,
+  isMarried: false,
+};
+
+console.log(
+  `Saya bernama ${profile.firstNama} ${profile.lastName}, umur saya adalah ${profile.age} tahun dan status menikah saya ${profile.isMarried}`
+);
